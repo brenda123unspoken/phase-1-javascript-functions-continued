c11b1b148139e08cd09bc08036cf78a6022509c5
Brenda Mukaya <brendamukaya@gmail.com>
2024-04-02 05:30:59 +0300

    modified and completed

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
 // code your solution here
+//saturday function and default activity
+function saturdayFun(activity = "roller-skate") {
+    
+      
+      return  "This Saturday , Iwant to ${activity}!";
+
+};
+
+
+
+console.log(saturdayFun()); 
+
+// Permitting the default activity to be overridden with "bathe my dog"
+console.log(saturdayFun("bathe my dog"));
+
+
+// Defines mondayWork function expression 
+const mondayWork = function (activity = "go to the office") {
+    return 'This Monday, I will ${activity}.'
+}
+console.log(mondayWork("work from home"))
+
+// Defines wrapAdjective function 
+//Defines wrapAdjective function 
+function wrapAdjective(symbol = "*") {
+    return function (adjective = "a hard worker") {
+        return  "You are ${symbol}${adjective}${symbol}!" ;
+    }
+}
+
+// Initialize the function with '*'
+const wrapWithAsterisk = wrapAdjective('*');
+
+// Call the created function with an adjective
+constresult  = wrapWithAsterisk("a hard worker");
+
+//call the created function with an adjective
+constresult  = wrapWithAsterisk('||a dedicated programmer||!')
+
+
+console.log(result); 
+    
+
+module.exports = {
+
+    saturdayFun,
+    mondayWork,
+    wrapAdjective
+};
